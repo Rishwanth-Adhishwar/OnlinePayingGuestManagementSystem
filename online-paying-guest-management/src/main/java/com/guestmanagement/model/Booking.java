9348bb28f15ec9d9863fcc91f573275ba1b62147
Subathra-subu <subathrasubu2004@gmail.com>
2026-03-12 22:45:50 +0530

    Updated Booking model and BookingService classes

--- online-paying-guest-management/src/main/java/com/guestmanagement/model/Booking.java
+++ online-paying-guest-management/src/main/java/com/guestmanagement/model/Booking.java
@@ -1,5 +1,62 @@
 package com.guestmanagement.model;
 
-public class Booking {
+import java.time.LocalDate;
 
+public class Booking {
+	
+	private static int idCounter = 1;
+	
+	private int bookingId;
+	private int tenantId;
+	private int roomId;
+	private LocalDate bookingDate;
+	private boolean status;
+	
+	public Booking() {
+		
+	}
+	
+	public Booking(int tenantId,int roomId){
+		
+		bookingId = idCounter++;
+		this.tenantId = tenantId;
+		this.roomId = roomId;
+		bookingDate = LocalDate.now();
+		status = true;
+		
+	}
+	
+	public int getBookingId() {
+		return bookingId;
+	}
+	
+	public int getTenantId() {
+	    return tenantId;
+	}
+
+	public int getRoomId() {
+	    return roomId;
+	}
+
+	public LocalDate getBookingDate() {
+	    return bookingDate;
+	}
+
+	public boolean isStatus() {
+	    return status;
+	}
+	
+	public void setStatus(boolean status) {
+	    this.status = status;
+	}
+	
+	@Override
+	public String toString() {
+	    return "Booking Details\n" +
+	           "Booking ID : " + getBookingId() + "\n" +
+	           "Tenant ID  : " + getTenantId() + "\n" +
+	           "Room ID    : " + getRoomId() + "\n" +
+	           "Booking Date : " + getBookingDate() + "\n" +
+	           "Status : " + (isStatus() ? "Active" : "Cancelled");
+	}
 }
--- online-paying-guest-management/src/main/java/com/guestmanagement/service/BookingService.java
+++ online-paying-guest-management/src/main/java/com/guestmanagement/service/BookingService.java
@@ -1,5 +1,60 @@
 package com.guestmanagement.service;
 
+import com.guestmanagement.model.Booking;
+
 public class BookingService {
+	
+	Booking[] booking = new Booking[100];
+	
+	int count = 0;
+	
+	public void bookRoom(int roomId,int tenantId) {
+		
+		if (count >= booking.length) {
+			System.out.println("Booking storage full");
+			return;
+		}
+		
+		booking[count] = new Booking(tenantId, roomId);
+		
+		count++;
+		
+		System.out.println("Booking created successfully. Booking ID: " + booking[count-1].getBookingId());
+		
+	}
+	
+	public void cancelBooking(int bookingId) {
+		
+		boolean found = false;
+		
+		for (int i = 0; i < count; i++) {
+			
+			if(bookingId == booking[i].getBookingId()) {
+				
+				booking[i].setStatus(false);
+				found = true;
+				break;
+			}
+		}
+		
+		if(found)
+			System.out.println("Booking Cancelled.");
+		else
+			System.out.println("Booking ID not found");
+		
+	}
+	
+	public void viewBookingDetails(int bookingId) {
+		
+		for (int i = 0; i < count; i++) {
+			
+			if(bookingId == booking[i].getBookingId()) {
+				
+				System.out.println(booking[i]);
+				return;
+			}
+		}
+		System.out.println("Booking ID not found");
+	}
 
 }
