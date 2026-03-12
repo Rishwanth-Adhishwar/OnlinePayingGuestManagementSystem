791320f3e5c678bc7091112c427c0fde2467488c
Rishwanth Adhishwar <krishwanthadhiswar@gmail.com>
2026-03-12 20:28:29 +0530

    MainApp - class has been Done

--- online-paying-guest-management/src/main/java/com/guestmanagement/MainApp.java
+++ online-paying-guest-management/src/main/java/com/guestmanagement/MainApp.java
@@ -1,9 +1,17 @@
 package com.guestmanagement;
 
+import com.guestmanagement.menu.MainMenu;
+
 public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		 System.out.println("==================================");
+	        System.out.println("  PG MANAGEMENT SYSTEM (CLI) ");
+	        System.out.println("==================================");
+
+	        MainMenu menu = new MainMenu();
+	        
 
 	}
 
