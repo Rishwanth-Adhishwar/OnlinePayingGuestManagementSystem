905db0283d3617b59b8ce604236962dc8e28465a
jeevapranesh005 <jeevapranesh.vrp@gmail.com>
2026-03-16 21:29:32 +0530

    room and property files updated

--- online-paying-guest-management/src/main/java/com/guestmanagement/model/Room.java
+++ online-paying-guest-management/src/main/java/com/guestmanagement/model/Room.java
@@ -1,5 +1,81 @@
 package com.guestmanagement.model;
 
 public class Room {
+	private int roomId;
+	private String roomType;
+	private double price;
+	private int capacity;
+	private boolean availability;
+	
+	
+	
+	public Room(int roomId, String roomType, double price, int capacity, boolean availability) {
+		this.roomId = roomId;
+		this.roomType = roomType;
+		this.price = price;
+		this.capacity = capacity;
+		this.availability = availability;
+	}
 
+
+
+	public int getRoomId() {
+		return roomId;
+	}
+
+
+
+	public void setRoomId(int roomId) {
+		this.roomId = roomId;
+	}
+
+
+
+	public String getRoomType() {
+		return roomType;
+	}
+
+
+
+	public void setRoomType(String roomType) {
+		this.roomType = roomType;
+	}
+
+
+
+	public double getPrice() {
+		return price;
+	}
+
+
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+
+
+	public int getCapacity() {
+		return capacity;
+	}
+
+
+
+	public void setCapacity(int capacity) {
+		this.capacity = capacity;
+	}
+
+
+
+	public boolean isAvailability() {
+		return availability;
+	}
+
+
+
+	public void setAvailability(boolean availability) {
+		this.availability = availability;
+	}
+
+	
 }
--- online-paying-guest-management/src/main/java/com/guestmanagement/service/PropertyService.java
+++ online-paying-guest-management/src/main/java/com/guestmanagement/service/PropertyService.java
@@ -1,5 +1,33 @@
 package com.guestmanagement.service;
 
-public class PropertyService {
+import java.util.ArrayList;
+import java.util.List;
 
+import com.guestmanagement.model.Property;
+
+public class PropertyService {
+	private List<Property> propertyList;
+	
+	public PropertyService(){
+	    propertyList = new ArrayList<>();
+	}
+	
+	public void addProperty(Property property){
+	    propertyList.add(property);
+	    System.out.println("Property Added Successfully");
+	}
+	
+	public void updateProperty(int propertyId, String name, String location){
+
+	    for(Property p : propertyList){
+
+	        if(p.getPropertyId() == propertyId){
+
+	            p.setPropertyName(name);
+	            p.setLocation(location);
+
+	            System.out.println("Property Updated Successfully");
+	        }
+	    }
+	}
 }
