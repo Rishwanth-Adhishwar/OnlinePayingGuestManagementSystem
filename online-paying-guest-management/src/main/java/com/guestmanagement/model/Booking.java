12f76fed3e6755b3b6513c85e02550c2343ce746
Jananisri14 <jananisathishkumar380@gmail.com>
2026-03-23 22:09:47 +0530

    Class Implementation

--- online-paying-guest-management/src/main/java/com/guestmanagement/model/Booking.java
+++ online-paying-guest-management/src/main/java/com/guestmanagement/model/Booking.java
@@ -1,5 +1,21 @@
 package com.guestmanagement.model;
 
-public class Booking {
 
+public class Booking {
+    public int    id;
+    public int    tenantId;
+    public int    roomId;
+    public String date;
+
+    public Booking(int id, int tenantId, int roomId, String date) {
+        this.id       = id;
+        this.tenantId = tenantId;
+        this.roomId   = roomId;
+        this.date     = date;
+    }
+
+    public String toString() {
+        return "BookingID:" + id + " | TenantID:" + tenantId
+             + " | RoomID:" + roomId + " | Date:" + date;
+    }
 }
