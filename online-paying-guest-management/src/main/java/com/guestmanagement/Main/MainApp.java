fcf45f9e33d6298f038fb911c7f1cf1b1b92698d
Rishwanth Adhishwar <krishwanthadhiswar@gmail.com>
2026-03-30 21:17:54 +0530

    Delete online-paying-guest-management directory
    duplicates

--- online-paying-guest-management/src/main/java/com/guestmanagement/Main/MainApp.java
+++
@@ -1,7 +0,0 @@
-package com.guestmanagement.Main;
-//main11
-public class MainApp {
-	public static void main(String[] args) {
-		
-	}
-}
--- online-paying-guest-management/src/main/java/com/guestmanagement/model/Message.java
+++
@@ -1,5 +0,0 @@
-package com.guestmanagement.model;
-
-public class Message {
-
-}
