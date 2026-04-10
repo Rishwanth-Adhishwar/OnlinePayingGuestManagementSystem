e2a729270e9fcc384379d1253245c8972ab223d0
jeevapranesh005 <jeevapranesh.vrp@gmail.com>
2026-04-10 06:19:05 +0530

    modified

--- OnlinePayingGuestManagementSystem/online-paying-guest-management/src/main/java/com/guestmanagement/database/Database.java
+++ OnlinePayingGuestManagementSystem/online-paying-guest-management/src/main/java/com/guestmanagement/database/Database.java
@@ -22,11 +22,11 @@ import java.sql.SQLException;
 public class Database {
     
     // Static variables / Constants for DB configuration
-    public static String HOST = " mysql-313d8acf-guestmanagementsystem-4.j.aivencloud.com";
-    public static String PORT = "27913";
-    public static String DATABASE = "guest_db";
-    public static String USER = "avnadmin";
-    public static String PASSWORD = "AVNS_qtP9PXrlBVRWIbjsSIq";
+    public static String HOST = " #";
+    public static String PORT = "#";
+    public static String DATABASE = "#";
+    public static String USER = "#";
+    public static String PASSWORD = "#";
     
     // Static method / Database connection
     public static Connection connect() {
